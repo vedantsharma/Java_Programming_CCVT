624ae0b94ce518e95695ae5394abdec007f7e4cb
vedantsharma <vedantsharma441@gmail.com>
2018-02-19 21:15:09 +0530

    comments added after print statement

--- CCVT_Code_Distribute/src/com/rt/cloning/TestClass.java
+++ CCVT_Code_Distribute/src/com/rt/cloning/TestClass.java
@@ -1,11 +1,7 @@
 package com.rt.cloning;
 
 public class TestClass {
-//changes done
-	/**
-	 * @param args
-	 * @throws CloneNotSupportedException 
-	 */
+
 	public static void main(String[] args) throws CloneNotSupportedException {
 		// TODO Auto-generated method stub
 	Department d1=new Department(7, "HR");
@@ -14,18 +10,26 @@ public class TestClass {
 	
 
 	System.out.println(e1);
+	//Employee id: 1 Employee Name: Ram Dep Name:HR Dep Id:7
 	System.out.println(e2);
+	//Employee id: 1 Employee Name: Ram Dep Name:HR Dep Id:7
 //	e2.setEid(3);
 	e2.setEname("Shyam");
 	e2.getDep().setDid(8);
 	e2.getDep().setDname("Acc");
 	System.out.println(e1);
+	//Employee id: 1 Employee Name: Ram Dep Name:HR Dep Id:7
 	System.out.println(e2);
+	//Employee id: 1 Employee Name: Shyam Dep Name:Acc Dep Id:8
 //new Employee().getEid();
 	System.out.println(e1 instanceof Cloneable);
+	//true
 	System.out.println(e1.clone()!=e1);
+	//true
 	System.out.println(e2.getClass()==e1.getClass());
+	//true
 	System.out.println(e1.equals(e2));
+	//true
 	}
 
 }
